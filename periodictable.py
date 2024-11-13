3154580b49973135e717fafb4712aa127be0e4a7
rfrankeltcsbell <bellmore+rfrankel@thecoderschool.com>
2024-11-13 21:29:45 +0000

    debugged errors

--- periodictable.py
+++ periodictable.py
@@ -6,27 +6,26 @@ import os
 # Function to create the structure of the csv files if they don't exist
 def create_csv_files():
   #Check if periodic_table_.csv exist, if not create it
-  if not os.path exist('periodic_table.csv'):
-    with open ('periodic_table.csv','w',newline='')as file:
-      writer= csv.DicWriter(file,fieldnames=['Symbol','Name','Atomic Number','Atomic Weight'])
-      writer.writerheader()
-      #Optinonally add some default elements
-      writer.writerow({'Symbol':'H','Name':'Hydrogen','Atomic Number'1,'Atomic Weight':1.008})
-      Writer.writerow({'Symbol':'O','Name':'Oxygen','Atomic Number'8,'Atomic Weight':15.999})
-      Writer.wrtierow({'Symbol':'C','Name':'Carbon','Atomic Number':6,'Atomic Weight':12.011})
-      Writer.writerow({'Symbol':'N','Name':'Nitrogen','Atomic Number':7,'Atomic Weight':14.007})
-      Writer.writerow({'Symbol':'Li','Name':'Lithium','Atomic Number':3,'Atomic Weight':6.9410})
-      print("Created Periodic_table.csv")
+  if not os.path.exists('periodic_table.csv'):
+      with open ('periodic_table.csv','w',newline='') as file:
+        writer= csv.DictWriter(file,fieldnames=['Symbol','Name','Atomic Number','Atomic Weight'])
+        writer.writeheader()
+        #Optinonally add some default elements
+        writer.writerow({'Symbol':'H','Name':'Hydrogen','Atomic Number':1,'Atomic Weight':1.008})
+        writer.writerow({'Symbol':'O','Name':'Oxygen','Atomic Number':8,'Atomic Weight':15.999})
+        writer.writerow({'Symbol':'C','Name':'Carbon','Atomic Number':6,'Atomic Weight':12.011})
+        writer.writerow({'Symbol':'N','Name':'Nitrogen','Atomic Number':7,'Atomic Weight':14.007})
+        writer.writerow({'Symbol':'Li','Name':'Lithium','Atomic Number':3,'Atomic Weight':6.9410})
+        print("Created Periodic_table.csv")
       
       # a file path is location on your computer
       # writer allows us to write into our file
   if not os.path.exists('compounds.csv'):
-    with open('compound.csv','w',newline=')as file:
+    with open('compound.csv','w',newline='') as file:
       writer = csv.DictWriter(file,fieldnames=['Compound Name','Base Elements','Proportions'])
-      writer.writerhead()
+      writer.writeheader()
       # Opionally add a default compound
-      writer.writerow(['Compound Name':,'Water','Base Elements':'H20','Proportions':'2 H,1 0'})
-      
+      writer.writerow({'Compound Name':'Water','Base Elements':'H20','Proportions':'2 H,1 0'})
       print ("Createrd compounds,csv")
       
       # Load periodic table from CSV
@@ -43,14 +42,14 @@ def load_elements(file_name):
     return elements
             
 def load_compounds(file_name):
-        compounds ={}
-        with open (file_name,'r')as file:
-            reader = csv.Dictreader(file) 
+        compounds =[]
+        with open (file_name,'r') as file:
+            reader = csv.DictReader(file) 
             for row in reader: 
                 compounds.append({
                 'name':row['Compound Name'],
                 'elements':row['Base Elements'],
-                'proportion':row['Proportions']
+                'proportions':row['Proportions']
                 })
         return compounds
                 
@@ -59,8 +58,14 @@ def load_compounds(file_name):
         #View periodic table
 def view_periodic_table(elements):
     for symbol,details in elements.items():
-    print(f"{symbol}: {details['name']}(Atomic Number):{details['atomic_number']},Atomic Weight:{details['atomic_weight']})    ")
+        print(f"{symbol}: {details['name']}(Atomic Number):{details['atomic_number']},Atomic Weight:{details['atomic_weight']})    ")
+    # Create new compound
+def create_compound(elements): 
+    compound_name = input("Enter the name of the new compound:")
+    compound_elements = input("Enter the elements(e.g.,H2O)")
+    proportions = input("Enter the proportions(e.g.,2 H,1 0)")
 
+    return {'Compound Name': compound_name,'Base Elements':compound_elements,'Proportions':proportions}
 # Save new compound to CSV
 def save_compound(compound,file_name):
     with open(file_name,'a',newline='')as file:
@@ -98,7 +103,7 @@ def menu(elements,compounds_file):
 
 if __name__ =="__main__":
     elements_file='periodic_table.csv'
-    compounds_file = 'compounds.csv'
+    compounds_file = 'compound.csv'
 
     # create CSV files if they don't exist
     create_csv_files()
